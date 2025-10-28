30ce80e49ee61bbe22ca82f11a8042555b47028e
BulusHamnu <hamnubulus@gmail.com>
2025-10-28 21:53:49 +0100

    Fix: fix withAuth middleware to handle jwt error gracefully

--- src/middlewares/withAuth.js
+++ src/middlewares/withAuth.js
@@ -4,7 +4,15 @@ import jwt from "jsonwebtoken";
 
 export default async function (req, res, next) {
   try {
-    const token = req.cookies.token;
+    const token = req.cookies?.token;
+
+    if (!token) {
+      logger.info("Token not provided.");
+      return res.status(401).json({
+        status: false,
+        message: "No token provided. Unauthorized.",
+      });
+    }
 
     const user = jwt.verify(token, env.SECRET_KEY);
     if (!user)
@@ -17,6 +25,20 @@ export default async function (req, res, next) {
     next();
   } catch (error) {
     logger.error(error);
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({
+        status: false,
+        message: "Token expired. Please log in again.",
+      });
+    }
+
+    if (error.name === "JsonWebTokenError") {
+      return res.status(401).json({
+        status: false,
+        message: "Invalid token. Unauthorized.",
+      });
+    }
+
     res.status(500).json({
       status: false,
       message: "An unexpected error occured.",
