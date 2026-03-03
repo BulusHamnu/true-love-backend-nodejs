bf69ddb3635792489e6e4b4ea74f15ac0d4725af
BulusHamnu <hamnubulus@gmail.com>
2026-03-03 22:50:11 +0100

    Refactored migration script for creating unique indexes

--- src/migrations/011_create_unique_indexes_on_user_profile_selfguidedprograms_table.js.js
+++ src/migrations/011_create_unique_indexes_on_user_profile_selfguidedprograms_table.js.js
@@ -1,129 +1,87 @@
 import Logger from "../utils/logger.js";
 
 /* Change or create new indexes for Users, SelfGuidedProgram and Profiles tables. */
+async function createUniqueIndex(db, collectionName, field) {
+  const collection = db.collection(collectionName);
+  const indexes = await collection.indexes();
+
+  const index = indexes.find((index) => index.key[field]);
+  if (!index?.unique) {
+    if (index) {
+      await collection.dropIndex(index.name);
+      Logger.info(`Old ${field} index was deleted.`);
+    }
+
+    const newIndex = await collection.createIndex(
+      { [field]: 1 },
+      { unique: true },
+    );
+
+    Logger.info(`New unique ${field} index created: ${newIndex}.`);
+  }
+}
+
+async function createUniquePartialIndexWithCleanup({
+  db,
+  collectionName,
+  field,
+  cleanupQuery,
+  unsetFields,
+  filterExpression,
+}) {
+  const collection = db.collection(collectionName);
+  const indexes = await collection.indexes();
+
+  const index = indexes.find((index) => index.key[field]);
+  if (!index?.unique || !index?.partialFilterExpression) {
+    if (index) {
+      await collection.dropIndex(index.name);
+      Logger.info(`Old ${field} index was deleted.`);
+    }
+
+    // Function for cleaning up old documents or unsetting fields that may cause duplicate key error.
+    await collection.updateMany(cleanupQuery, { $unset: unsetFields });
+    const newIndex = await collection.createIndex(
+      { [field]: 1 },
+      {
+        unique: true,
+        partialFilterExpression: { [field]: filterExpression },
+      },
+    );
+
+    Logger.info(`New unique ${field} index created: ${newIndex}.`);
+  }
+}
+
 export async function up(db) {
-  const userCollection = db.collection("users");
-  const usersIndexes = await userCollection.indexes();
-
   // Users email index migration
-  const emailIndex = usersIndexes.find((index) => index.key["email"]);
-  if (!emailIndex?.unique) {
-    if (emailIndex) {
-      await userCollection.dropIndex(emailIndex.name);
-      Logger.info("Old email index was deleted.");
-    }
+  await createUniqueIndex(db, "users", "email");
 
-    const newEmailIndex = await userCollection.createIndex(
-      { email: 1 },
-      { unique: true },
-    );
-    Logger.info(`New unique email index created: ${newEmailIndex}.`);
-  }
-
-  // Users googleId index migration
-  const googleIdIndex = usersIndexes.find(
-    (index) => index.key["google.googleId"],
-  );
-
-  if (!googleIdIndex?.unique || !googleIdIndex?.partialFilterExpression) {
-    if (googleIdIndex) {
-      await userCollection.dropIndex(googleIdIndex.name);
-      Logger.info("Old google.googleId index was deleted.");
-    }
-
-    // Because old document were created with googleId and idToken having empty string as default values, we need to delete them so that we will not have duplicate key error.
-    await userCollection.updateMany(
-      { "google.googleId": "" },
-      {
-        $unset: { "google.googleId": "", "google.idToken": "" },
-      },
-    );
-
-    const newGoogleIdIndex = await userCollection.createIndex(
-      { "google.googleId": 1 },
-      {
-        unique: true,
-        partialFilterExpression: { "google.googleId": { $exists: true } },
-      },
-    );
-    Logger.info(`New unique googleId index created: ${newGoogleIdIndex}.`);
-  }
-
-  // selfGuidedPrograms userId index migration
-  const selfGuidedCollections = db.collection("selfguidedprograms");
-  const selfGuidedIndexes = await selfGuidedCollections.indexes();
-  const selfGuidedUserIdIndex = selfGuidedIndexes.find(
-    (index) => index.key["userId"],
-  );
-  if (!selfGuidedUserIdIndex?.unique) {
-    if (selfGuidedUserIdIndex) {
-      await selfGuidedCollections.dropIndex(selfGuidedUserIdIndex.name);
-      Logger.info("Old selfGuidedPrograms userId index was deleted.");
-    }
-
-    const newUserIdIndex = await selfGuidedCollections.createIndex(
-      { userId: 1 },
-      { unique: true },
-    );
-    Logger.info(
-      `New unique selfGuidedPrograms userId index created: ${newUserIdIndex}.`,
-    );
-  }
-
-  // stripeCustomerId index migration
-  const profileCollection = db.collection("profiles");
-  const profilesIndexes = await profileCollection.indexes();
-
-  const stripeCustomerIdIndex = profilesIndexes.find(
-    (index) => index.key["stripeCustomerId"],
-  );
-
-  if (
-    !stripeCustomerIdIndex?.unique ||
-    !stripeCustomerIdIndex?.partialFilterExpression
-  ) {
-    if (stripeCustomerIdIndex) {
-      await profileCollection.dropIndex(stripeCustomerIdIndex.name);
-      Logger.info("Old profiles stripeCustomerId index was deleted.");
-    }
-
-    // Because old document were created with stripeCustomerId having empty string as default value, we need to delete it, so that we will not have duplicate key failure.
-    await profileCollection.updateMany(
-      { stripeCustomerId: "" },
-      {
-        $unset: { stripeCustomerId: "" },
-      },
-    );
-
-    const newStripeCustomerIdIndex = await profileCollection.createIndex(
-      { stripeCustomerId: 1 },
-      {
-        unique: true,
-        partialFilterExpression: { stripeCustomerId: { $exists: true } },
-      },
-    );
-    Logger.info(
-      `New unique profiles stripeCustomerId index created: ${newStripeCustomerIdIndex}.`,
-    );
-  }
+  // SelfGuidedPrograms userId index migration
+  await createUniqueIndex(db, "selfguidedprograms", "userId");
 
   // Profile userId migration
-  const profileUserIdIndex = profilesIndexes.find(
-    (index) => index.key["userId"],
-  );
+  await createUniqueIndex(db, "profiles", "userId");
 
-  if (!profileUserIdIndex?.unique) {
-    if (profileUserIdIndex) {
-      await profileCollection.dropIndex(profileUserIdIndex.name);
-      Logger.info("Old profiles userId index was deleted.");
-    }
+  // Users googleId index migration
+  await createUniquePartialIndexWithCleanup({
+    db,
+    collectionName: "users",
+    field: "google.googleId",
+    cleanupQuery: { "google.googleId": "" },
+    unsetFields: { "google.googleId": "", "google.idToken": "" },
+    filterExpression: { "google.googleId": { $exists: true } },
+  });
 
-    const newUserIdIndex = await profileCollection.createIndex(
-      { userId: 1 },
-      { unique: true },
-    );
-    Logger.info(`Unique profiles userId index created: ${newUserIdIndex}.`);
-  }
+  // Profiles userId index migration
+  await createUniquePartialIndexWithCleanup({
+    db,
+    collectionName: "profiles",
+    field: "stripeCustomerId",
+    cleanupQuery: { stripeCustomerId: "" },
+    unsetFields: { stripeCustomerId: "" },
+    filterExpression: { stripeCustomerId: { $exists: true } },
+  });
 
   Logger.info(
     "email, googleId, selfGuidedProgram userId, profile userId, and stripeCustomerId Indexes migration completed.",
